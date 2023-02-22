8c7fc4829400a1634578616c5f54104c6aadb408
Prasad Khalkar <prasadkhalkar5@gmail.com>
2023-02-23 00:17:15 +0530

    Deleted unnecessary files

--- tnp_portal_server/tnp_portal/src/main/java/com/example/tnp_portal/repository/ICompanyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.tnp_portal.repository;
-
-import com.example.tnp_portal.entity.Company;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ICompanyRepository extends MongoRepository<Company, ObjectId> {
-}
